2869ad538321ac79f781b5bfde0a2b9e8c70aa72
Nic <nicholas.vickery-wilson@paduafinancialgroup.com.au>
2020-07-19 01:29:20 +1000

    Add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,13 @@ import express from "express";
 import { connect } from "mongodb";
 import { config } from "dotenv";
 import { v4 as uuid } from "uuid";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 config();
 const port = 9000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-0fcm1.mongodb.net/polls?retryWrites=true&w=majority`;
@@ -12,7 +17,7 @@ app.get("/", (req, res) => {
   res.send("The sedulous hyena ate the antelope!");
 });
 
-app.listen(port, (err) => {
+app.listen(port, async (err) => {
   if (err) {
     return console.error(err);
   }
@@ -34,26 +39,27 @@ app.listen(port, (err) => {
         const foundPolls = await polls.find().toArray();
 
         console.log(foundPolls);
-
         res.send(foundPolls.toString());
       });
 
       app.get("/polls/:id", (req, res) => {
         const { id } = req.params;
 
-        polls.findOne({ title: "cunt" }).then((poll) => {
+        polls.findOne({ pollId: id }).then((poll) => {
           console.log(poll);
 
           res.send("Go fuck yourself");
         });
       });
 
-      app.post("/polls", (req, res) => {
+      app.post("/poll", jsonParser, (req, res) => {
         const pollId = uuid();
         polls
           .insertOne({ ...req.body, pollId })
           .then((result: any) => {
-            console.log(result);
+            console.log(result.ops);
+
+            res.send(JSON.stringify(result.ops));
           })
           .catch((error: any) => console.error(error));
       });
